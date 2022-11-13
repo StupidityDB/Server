91cbcded8fddee76bd420f9a7bb23de781bba2c2
RGBCube <RGBCube@users.noreply.github.com>
2022-11-13 17:51:56 +0300

    Use the callback code

--- stupidity_db_server/routes/authorize.py
+++ stupidity_db_server/routes/authorize.py
@@ -2,7 +2,7 @@ __all__ = ("router",)
 
 from inspect import cleandoc
 
-from fastapi import APIRouter, Depends, status
+from fastapi import APIRouter, Depends, Query, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi_discord import DiscordOAuthClient as DiscordOAuth2Client
 
@@ -31,27 +31,48 @@ async def authorize(
 
 
 @router.get(
-    "/success",
-    summary="Success page after linking your Discord account.",
-    description="This endpoint says that you have successfully linked your Discord account.",
-    response_description="Success page after linking your Discord account.",
+    "/callback",
+    summary="Callback for Discord OAuth2.",
+    description="You will be redirected here after authorizing with Discord.",
+    response_description="HTML page with information about the authorization.",
     response_class=HTMLResponse,
     responses=generate_example(
         cleandoc(
             """
             <h1>Success!</h1>
-            <p>You are have authorized StupidityDB successsfully.</p>
+            <p>You are have authorized StupidityDB successfully.</p>
             """
         ),
         html=True
     )
 )
-async def authorize_success() -> HTMLResponse:
+async def authorize_success(
+    *,
+    oauth2_: DiscordOAuth2Client = Depends(oauth2.get_oauth2),
+    code: str | None = Query(
+        default=None,
+        description="The authorization code returned by Discord.",
+        example="Waf3Tvx8kdos5hta3gcJ8GndJSoBqI"
+    )
+) -> HTMLResponse:
+    if code is None:
+        return HTMLResponse(
+            content=cleandoc(
+                """
+                <h1>Error!</h1>
+                <p>You did not provide an authorization code.</p>
+                """
+            ),
+            status_code=status.HTTP_401_UNAUTHORIZED
+        )
+
+    token, refresh_token = await oauth2_.get_access_token(code)
+    # Now what?
     return HTMLResponse(
         cleandoc(
             """
             <h1>Success!</h1>
-            <p>You are have authorized StupidityDB successsfully.</p>
+            <p>You are have authorized StupidityDB successfully.</p>
             """
         )
     )
