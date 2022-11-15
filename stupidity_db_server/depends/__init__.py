ce15c42fd45e0c8eeafc0f7f6d9ef2860ba23695
RGBCube <RGBCube@users.noreply.github.com>
2022-11-15 17:12:50 +0300

    Fix imports in depends/

--- stupidity_db_server/depends/__init__.py
+++ stupidity_db_server/depends/__init__.py
@@ -1,4 +1,1 @@
-from stupidity_db_server.depends.get.db import *
-from stupidity_db_server.depends.get.oauth import *
-from stupidity_db_server.depends.get.redis import *
-from . import oauth, params
+from . import get, oauth, params
