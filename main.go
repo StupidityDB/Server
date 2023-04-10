0d66b86860446743689f58af31bec996c2844387
RGBCube <RGBCube@users.noreply.github.com>
2023-04-10 22:14:34 +0300

    Chore: Use chi/CORS

--- main.go
+++ main.go
@@ -6,33 +6,25 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi"
+	"github.com/go-chi/cors"
 
 	"server-go/config"
 )
 
-type StupidityDBServer struct {
-	*chi.Mux
-}
-
-func (s *StupidityDBServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	// CORS
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Access-Control-Allow-Headers", "*")
-	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-
-	s.Mux.ServeHTTP(w, r)
-}
-
 func main() {
-	root := &StupidityDBServer{chi.NewRouter()}
+	root := chi.NewRouter()
+	root.Use(
+		cors.Handler(
+			cors.Options{
+				AllowedOrigins: []string{"*"},
+				AllowedMethods: []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+				AllowedHeaders: []string{"*"},
+			},
+		),
+	)
 
-	api := chi.NewRouter()
-	root.Mount("/api", api)
-
-	apiUsers := chi.NewRouter()
-	api.Mount("/users", apiUsers)
-
-	if err := http.ListenAndServe(":"+config.Config.Port, root); err != nil && !errors.Is(err, http.ErrServerClosed) {
+	if err := http.ListenAndServe(":"+config.Config.Port, root); err != nil &&
+		!errors.Is(err, http.ErrServerClosed) {
 		log.Fatal(err)
 	}
 }
